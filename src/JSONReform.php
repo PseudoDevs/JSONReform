9c073d3ac5b3b4cc12e9cea30dc458dbe745e24d
PseudoDevs <johnlester.mychannelnow@gmail.com>
2023-04-21 01:15:30 +0800

    Adding a method for reading a json file

--- src/JSONReform.php
+++ src/JSONReform.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace iamjohndev;
 
 use InvalidArgumentException;
@@ -9,9 +7,10 @@ use InvalidArgumentException;
 /**
  * A simple class for reading and formatting JSON data.
  */
-class JSONReform
+class JsonReader
 {
-    protected array $data;
+    /** @var array */
+    private $data;
 
     /**
      * Creates a new JsonReader instance from a JSON string.
@@ -24,13 +23,35 @@ class JSONReform
     }
 
     /**
+     * Creates a new JsonReader instance from a JSON file.
+     *
+     * @param string $filename The path to the JSON file to read.
+     * @return JsonReader The JsonReader instance created from the file.
+     * @throws InvalidArgumentException If the file does not exist or cannot be read.
+     */
+    public static function fromFile(string $filename): JsonReader
+    {
+        if (!is_file($filename) || !is_readable($filename)) {
+            throw new InvalidArgumentException("File not found or cannot be read: $filename");
+        }
+
+        $json = file_get_contents($filename);
+
+        if ($json === false) {
+            throw new InvalidArgumentException("Failed to read file: $filename");
+        }
+
+        return new self($json);
+    }
+
+    /**
      * Gets the value at the specified path in the JSON data.
      *
      * @param string $path The path to the value, in dot notation.
      * @param mixed $default The default value to return if the path is invalid.
      * @return mixed The value at the specified path, or the default value if the path is invalid.
      */
-    public function getValue(string $path, mixed $default = null): mixed
+    public function getValue(string $path, $default = null)
     {
         $keys = explode('.', $path);
         $value = $this->data;
@@ -39,7 +60,6 @@ class JSONReform
             if (!isset($value[$key])) {
                 return $default;
             }
-
             $value = $value[$key];
         }
 
@@ -51,16 +71,18 @@ class JSONReform
      *
      * @param string $format The format to use (one of 'json', 'pretty', 'minified').
      * @return string The JSON string in the specified format.
-     *
-     * @throws \InvalidArgumentException if an invalid format is provided.
      */
     public function format(string $format = 'json'): string
     {
-        return match ($format) {
-            'json' => json_encode($this->data),
-            'pretty' => json_encode($this->data, JSON_PRETTY_PRINT),
-            'minified' => json_encode($this->data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
-            default => throw new InvalidArgumentException("Invalid format: $format"),
-        };
+        switch ($format) {
+            case 'json':
+                return json_encode($this->data);
+            case 'pretty':
+                return json_encode($this->data, JSON_PRETTY_PRINT);
+            case 'minified':
+                return json_encode($this->data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+            default:
+                throw new InvalidArgumentException("Invalid format: $format");
+        }
     }
 }
